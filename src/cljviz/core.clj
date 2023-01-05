45dd60a53cc7035f14f2b77cafcc60fbafc2c2c3
Juha Kairamo <juha.kairamo@solita.fi>
2023-01-05 13:30:57 +0200

    input argument as file

--- src/cljviz/core.clj
+++ src/cljviz/core.clj
@@ -32,21 +32,25 @@
 
 
 (defn -main
-  "Attempt to turn clj-kondo analysis output to plantuml diagram. Input argumemnt clj-file or directory, output plantuml description."
+  "Attempt to turn clj-kondo analysis output to plantuml diagram. Input argument clj-file or directory, output plantuml description."
   [& args]
-  (if args
-    ((println "@startuml")
-     (apply println (map #(create-plantuml-object %) (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj"))))))
-     (println "@enduml"))
-    (println "Must have at least one argument!")))
+  (let [f (first args)]
+        (if f 
+          ((println "@startuml")
+           (apply println (map #(create-plantuml-object %) (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis f))))))
+           (println "@enduml"))
+          (println "Must have at least one argument!"))))
 ;;(println "Need an input clj-file or directory")))
 
 (comment
-  (-main "test")
+  (-main "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj") 
+  (-main "/home/juhakairamo/Projects/clojure/cljviz/src")
   (-main)
   (println \u0022)
   (run-lint-analysis "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj")
   (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj")))
+  (group-by :ns (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj"))))
+  (group-by :ns [{:ns 1 :a 2} {:ns 4 :c 45} {:ns 1 :b 2} {:ns 3 :a 2}])
   (merge {:a 1 :b 2} {:a 2 :c 3})
   (select-keys {:ns "test" :b 2 :c 3} [:ns])
   (map val (select-keys {:ns "test" :b 2 :c 3} [:ns]))
