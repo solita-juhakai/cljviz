0af56cb4bec4488ff754145356ac1bfe02289936
Juha Kairamo <juha.kairamo@solita.fi>
2023-03-24 16:47:52 +0200

    still WIP for graphviz support

--- src/cljviz/core.clj
+++ src/cljviz/core.clj
@@ -1,34 +1,29 @@
 (ns cljviz.core
   (:require [cljviz.util.lint :refer [run-lint-analysis]]
-            [cljviz.util.utils :refer [filter-usage-var-defs
-                                       filter-var-def-keys rand-color]]
-            [cljviz.writer.dotwriter :refer [create-pl-links
-                                            create-pl-ob-package]]
+            [cljviz.util.utils :refer [filter-usage-var-defs]]
+            [cljviz.writer.dotwriter :refer [main-dot-writer]]
+            [cljviz.writer.plwriter :refer [main-pl-writer]]
             [clojure.string :as string])
   (:gen-class))
 
 (defn -main
-  "Cljviz turns clj-kondo analysis output to plantuml diagram. Input argument clj-file or directory, output plantuml description."
+  "Cljviz turns clj-kondo analysis output to plantuml diagram. First input argument clj-file or directory, second output type (pl or gv), output is string presentation of chosen output type."
   [& args]
   (let [f (first args)
-        ma (:analysis (run-lint-analysis f))
-        m-d (:var-definitions ma)
-        m-u (:var-usages ma)]
+        o (second args)]
+        ;; ma (:analysis (run-lint-analysis f))
+        ;; m-d (:var-definitions ma)
+        ;; m-u (:var-usages ma)]
     (if f
-      (do
-        (println "@startuml" (last (string/split f #"/")))
-        (apply println (map #(create-pl-ob-package %) (group-by :ns (filter-var-def-keys m-d))))
-        (apply println
-               (map #(str (first (nth % 0)) "-[" (rand-color) ",thickness=" (nth % 1) "]->" (second (nth % 0)) ": " (nth % 1) "\n")
-                    (frequencies
-                     (filter identity (map #(create-pl-links %)
-                                           (filter identity (map #(filter-usage-var-defs % m-d) m-u)))))))
-        (println "@enduml"))
+      (cond
+        (= o "pl") (main-pl-writer f)
+        (= o "gv") (main-dot-writer f)
+        :else (println "Need proper output type"))
       (println "Need an input clj-file or directory"))))
 
 (comment
-  (-main "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj")
-  (-main "/home/juhakairamo/Projects/clojure/cljviz/src")
+  (-main "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj" "pl")
+  (-main "/home/juhakairamo/Projects/clojure/cljviz/src" "gv")
   (-main "/home/juhakairamo/Projects/clojure/aoc2022/src")
   (-main "/home/juhakairamo/Projects/clojure/xtdb-inspector/src")
   (-main "/home/juhakairamo/Projects/clojure/tab/src")
--- src/cljviz/writer/dotwriter.clj
+++ src/cljviz/writer/dotwriter.clj
@@ -1,6 +1,7 @@
 (ns cljviz.writer.dotwriter 
   (:require [cljviz.util.lint :refer [run-lint-analysis]]
-            [cljviz.util.utils :refer [filter-var-def-keys m-wonky-hash]]
+            [cljviz.util.utils :refer [filter-usage-var-defs
+                                       filter-var-def-keys m-wonky-hash rand-color]]
             [clojure.string :as string]))
 
 (defn create-dot-node "Create dot node from a map M :name, :defined-by and :ns key" [m]
@@ -10,14 +11,19 @@
   ;; testi ng}
   (let [dn (str (m :name))
         ds (last (string/split (name (m :defined-by)) #"/"))
-;        i (str (m :name) " <<" (last (string/split (name (m :defined-by)) #"/")) ">>")
+        node (str dn "<<" ds ">>")
         pi (m-wonky-hash (str (m :name) "_" (m :ns)))
         doc (m :doc)]
     (when (not (= "declare" ds))
-      (str "object " \u0022 dn " <<" ds ">>" \u0022 " as " pi (when doc (str " {\n" doc "\n}")) "\n"))))
+      (str pi 
+           "[\n"
+           "id=" pi "\n"
+           "label="\u0022 node \u0022"\n"
+           (when doc (str "doc=" \u0022 doc \u0022 "\n"))
+           "];\n"))))
 
 (comment
-  (map #(apply println (create-dot-node %)) (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj")))))
+  (map #(apply println (create-dot-node %)) (filter-var-def-keys   (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj"))))) 
   (create-dot-node {:name "tes-t" :doc "foo" :defined-by "clojure.core/defn" :ns "cljvis.core"})
   (create-dot-node {:name "tes-t" :doc "foo" :defined-by "clojure.core/declare" :ns "cljvis.core"})
   (def t-m '{:col 1
@@ -36,18 +42,20 @@
              :row 16})
   (m-wonky-hash (str (t-m :name) "_" (t-m :ns)))
   (compare "declare" "declare"))
+  
 
 
-(defn create-pl-ob-package "Create plantuml package section from vector V with first :ns and second map of vars" [v]
+(defn create-dot-subgraph "Create dot subgraph from vector V with first :ns and second map of vars" [v]
   (let [i (name (first v))
+        ri (string/replace i #"\." "_")
         m (second v)]
-    (str "package " i " {" "\n" (apply str (into #{} (map #(create-dot-node %) m))) " }" "\n")))
+    (str "subgraph cluster_" ri " {" "\n" (apply str (into #{} (map #(create-dot-node %) m))) "\n label=" \u0022 i \u0022 "}" "\n")))
 
 (comment
-  (map #(create-pl-ob-package %) (group-by :ns (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/aoc2022/src")))))))
+  (map #(create-dot-subgraph %) (group-by :ns (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/aoc2022/src")))))))
 
 
-(defn create-pl-links "Provides Plantuml relationship section links based on map M :from-var :from :name and :to values" [m]
+(defn create-dot-links "Provides dot relationship section links based on map M :from-var :from :name and :to values" [m]
   (let [frnname (m :from-var)
         frnnamens (m :from)
         tn (m :name)
@@ -55,37 +63,52 @@
     (when (and tn tns frnname frnnamens)
       (let [fr (m-wonky-hash (str frnname "_" frnnamens))
             to (m-wonky-hash (str tn "_" tns))]
-        (when (some? to) (list fr to))))))
+        (when (some? to) (str fr "->" to ";"))))))
 
 (comment
-  (create-pl-links '{:end-row 102,
-                     :name-end-col 28,
-                     :name-end-row 102,
-                     :name-row 102,
-                     :name tes-t,
-                     :filename "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj",
-                     :from cljviz.core,
-                     :col 26,
-                     :name-col 26,
-                     :from-var filter-from-vars,
-                     :end-col 28,
-                     :row 102,
-                     :to cljviz.core})
-  (map #(create-pl-ob-package %) (group-by :ns (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/aoc2022/src"))))))
-  (create-pl-links '{:arity 1
-                     :col 28
-                     :end-col 42
-                     :end-row 115
-                     :filename
-                     "/home/juhakairamo/Projects/clojure/aoc2022/src/aoc2022/aoc22_8.clj"
-                     :fixed-arities #{1}
-                     :from aoc2022.aoc22-8
-                     :from-var part-1
-                     :name parse-intsXXX
-                     :name-col 29
-                     :name-end-col 39
-                     :name-end-row 115
-                     :name-row 115
-                     :row 115
-                     :to aoc2022.aoc22-8}))
+  (create-dot-links '{:end-row 102,
+                      :name-end-col 28,
+                      :name-end-row 102,
+                      :name-row 102,
+                      :name tes-t,
+                      :filename "/home/juhakairamo/Projects/clojure/cljviz/src/cljviz/core.clj",
+                      :from cljviz.core,
+                      :col 26,
+                      :name-col 26,
+                      :from-var filter-from-vars,
+                      :end-col 28,
+                      :row 102,
+                      :to cljviz.core})
+  (map #(create-dot-subgraph %) (group-by :ns (filter-var-def-keys (:var-definitions (:analysis (run-lint-analysis "/home/juhakairamo/Projects/clojure/aoc2022/src"))))))
+  (create-dot-links '{:arity 1
+                      :col 28
+                      :end-col 42
+                      :end-row 115
+                      :filename
+                      "/home/juhakairamo/Projects/clojure/aoc2022/src/aoc2022/aoc22_8.clj"
+                      :fixed-arities #{1}
+                      :from aoc2022.aoc22-8
+                      :from-var part-1
+                      :name parse-intsXXX
+                      :name-col 29
+                      :name-end-col 39
+                      :name-end-row 115
+                      :name-row 115
+                      :row 115
+                      :to aoc2022.aoc22-8}))
+
+(defn main-dot-writer "Main writer for graphviz output" [f]
+  (let [ma (:analysis (run-lint-analysis f))
+        m-d (:var-definitions ma)
+        m-u (:var-usages ma)]
+    (do
+      (println "digraph" (last (string/split f #"/")) "{")
+      (apply println (map #(create-dot-subgraph %) (group-by :ns (filter-var-def-keys m-d))))
+      #_(apply println
+               (map #(str (first (nth % 0)) "-[" (rand-color) ",thickness=" (nth % 1) "]->" (second (nth % 0)) ": " (nth % 1) "\n")
+                    (frequencies
+                     (filter identity (map #(create-dot-links %)
+                                           (filter identity (map #(filter-usage-var-defs % m-d) m-u)))))))
+      (apply println (filter identity (map #(create-dot-links %) (filter identity (map #(filter-usage-var-defs % m-d) m-u)))))
+      (println "}"))))
 
