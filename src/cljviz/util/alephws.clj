9c672dca01ffc66af70a9d8d09a4be2a36fd9048
Juha Kairamo <juha.kairamo@solita.fi>
2023-05-11 15:08:31 +0300

    first working version with aleph for sending dot via web socket

--- src/cljviz/util/alephws.clj
+++ src/cljviz/util/alephws.clj
@@ -28,28 +28,28 @@
     (s/connect socket socket)
     non-websocket-request))
 
-(defn echo-handler
-  "The previous handler blocks until the websocket handshake completes, which unnecessarily
-   takes up a thread.  This accomplishes the same as above, but asynchronously. "
-  [req]
-  (-> (http/websocket-connection req)
-      (d/chain
-       (fn [socket]
-         (s/connect socket socket)))
-      (d/catch
-       (fn [_]
-         non-websocket-request))))
+;; (defn echo-handler
+;;   "The previous handler blocks until the websocket handshake completes, which unnecessarily
+;;    takes up a thread.  This accomplishes the same as above, but asynchronously. "
+;;   [req]
+;;   (-> (http/websocket-connection req)
+;;       (d/chain
+;;        (fn [socket]
+;;          (s/connect socket socket)))
+;;       (d/catch
+;;        (fn [_]
+;;          non-websocket-request))))
 
-(defn echo-handler
-  "This is another asynchronous handler, but uses `let-flow` instead of `chain` to define the
-   handler in a way that at least somewhat resembles the synchronous handler."
-  [req]
-  (->
-   (d/let-flow [socket (http/websocket-connection req)]
-               (s/connect socket socket))
-   (d/catch
-    (fn [_]
-      non-websocket-request))))
+;; (defn echo-handler
+;;   "This is another asynchronous handler, but uses `let-flow` instead of `chain` to define the
+;;    handler in a way that at least somewhat resembles the synchronous handler."
+;;   [req]
+;;   (->
+;;    (d/let-flow [socket (http/websocket-connection req)]
+;;                (s/connect socket socket))
+;;    (d/catch
+;;     (fn [_]
+;;       non-websocket-request))))
 
 ;; to represent all the different chat rooms, we use an **event bus**, which is simple
 ;; implementation of the publish/subscribe model
@@ -85,46 +85,62 @@
         ;; A ring handler expects some sort of HTTP response, so just give it `nil`
                             nil))))
 
+(def conns (atom []))
+
+(defn ws-handler
+  [req]
+  (d/let-flow [conn (d/catch
+                     (http/websocket-connection req)
+                     (fn [_] nil))]
+              (if-not conn
+                non-websocket-request
+                (d/let-flow [] (
+                                (swap! conns conj conn)
+                                (s/connect conn conn)
+                                )nil))))
+
+(defn send-all [msg]
+  (doseq [conn @conns]
+    (s/put! conn msg)))
+
 (def handler
   (params/wrap-params
    (ring/ring-handler
     (ring/router
      [["/echo" echo-handler]
-      ["/chat" chat-handler]])
+      ["/chat" chat-handler]
+      ["/ws" ws-handler]])
     (ring/create-default-handler))))
 
-;;(def s (http/start-server handler {:port 10001}))
-
-(defn start-websocket-server "" [port]
-  (http/start-server handler {:port port}))
+(def s (http/start-server handler {:port 3000}))
 
 ;; Here we `put!` ten messages to the server, and read them back again
-;; (let [conn @(http/websocket-client "ws://localhost:10000/echo")]
+(let [conn @(http/websocket-client "ws://localhost:10000/echo")]
 
-;;   (s/put-all! conn
-;;               (->> 10 range (map str)))
+  (s/put-all! conn
+              (->> 10 range (map str)))
 
-;;   (->> conn
-;;        (s/transform (take 10))
-;;        s/stream->seq
-;;        doall))    ;=> ("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
+  (->> conn
+       (s/transform (take 10))
+       s/stream->seq
+       doall))    ;=> ("0" "1" "2" "3" "4" "5" "6" "7" "8" "9")
 
-;; ;; Here we create two clients, and have them speak to each other
-;; (let [conn1 @(http/websocket-client "ws://localhost:10000/chat")
-;;       conn2 @(http/websocket-client "ws://localhost:10000/chat")]
+;; Here we create two clients, and have them speak to each other
+(let [conn1 @(http/websocket-client "ws://localhost:10000/chat")
+      conn2 @(http/websocket-client "ws://localhost:10000/chat")]
 
-;;   ;; sign our two users in
-;;   (s/put-all! conn1 ["shoes and ships" "Alice"])
-;;   (s/put-all! conn2 ["shoes and ships" "Bob"])
+  ;; sign our two users in
+  (s/put-all! conn1 ["shoes and ships" "Alice"])
+  (s/put-all! conn2 ["shoes and ships" "Bob"])
 
-;;   (s/put! conn1 "hello")
+  (s/put! conn1 "hello")
 
-;;   (println @(s/take! conn1))                                  ;=> "Alice: hello"
-;;   (println @(s/take! conn2))                                  ;=> "Alice: hello"
+  (println @(s/take! conn1))                                  ;=> "Alice: hello"
+  (println @(s/take! conn2))                                  ;=> "Alice: hello"
 
-;;   (s/put! conn2 "hi!")
+  (s/put! conn2 "hi!")
 
-;;   (println @(s/take! conn1))                                  ;=> "Bob: hi!"
-;;   (println @(s/take! conn2)))                                 ;=> "Bob: hi!"
+  (println @(s/take! conn1))                                  ;=> "Bob: hi!"
+  (println @(s/take! conn2)))                                 ;=> "Bob: hi!"
 
-;; (.close s)
+(.close s)
